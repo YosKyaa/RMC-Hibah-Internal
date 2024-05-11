b8ba3ffa3d13652c5918f2eeb9cbdf229650e07f
Abay <94115323+Adamrakb@users.noreply.github.com>
2024-05-11 16:11:10 +0700

    pict

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -67,9 +67,9 @@
                             </div>
                             <div class="col-sm-5 text-center text-sm-left">
                                 <div class="card-body pb-0 px-0 px-md-4">
-                                    <img src="../assets/img/illustrations/man-with-laptop-light.png" height="140"
-                                        alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png"
-                                        data-app-light-img="illustrations/man-with-laptop-light.png">
+                                    <img src="../assets/img/ANNOUNCEMENT.png" height="140"
+                                        alt="View Badge User" data-app-dark-img="assets/img/ANNOUNCEMENT.png"
+                                        data-app-light-img="assets/img/ANNOUNCEMENT.png">
                                 </div>
                             </div>
                         </div>
