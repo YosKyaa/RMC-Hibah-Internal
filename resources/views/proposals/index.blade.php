9855625217f23dc45f541f90c46c5ce5b2fa8c46
Abay <94115323+Adamrakb@users.noreply.github.com>
2024-05-27 23:04:29 +0700

    update submit proposal

--- resources/views/proposals/index.blade.php
+++ resources/views/proposals/index.blade.php
@@ -78,60 +78,70 @@
             padding: 0;
             list-style: none;
         }
+        
     </style>
 @endsection
 
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
+                <div class="order-4 mb-4 full-width">
+                    <div class="card">
+                        <div class="d-flex align-items-end row">
+                            <div class="col-sm-5 text-center text-sm-left">
+                                <div class="card-body pb-0">
+                                    <img src="../../assets/img/sitting-girl-with-laptop-light.png" height="140"
+                                        alt="Target User" data-app-dark-img="/sitting-girl-with-laptop-light.png"
+                                        data-app-light-img="/sitting-girl-with-laptop-light.png">
+                                </div>
+                            </div>
+                            <div class="col-sm-7">
+                                <div class="card-body">
+                                    <h5 class="card-title">Hi Reviewers!</h5>
+                                    <p class="mb-4">You have 12 task to finish today, Your already completed 189 task good
+                                        job.</p>
 
-        <h4 class="py-3 mb-4">
-            Submit Proposal
-        </h4>
+                                    <span class="badge bg-label-primary">78% of target</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <ul class="nav nav-pills flex-column flex-sm-row mb-4">
+                        <li class="nav-item"><a class="nav-link active" href="../user-proposals"><i class="bx bx-user me-1"></i> Upload</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../user-proposals/create"><i class="bx bx-group me-1"></i> Progres</a></li>
+                    </ul>
+                </div>
+            
 
         <!-- Timeline Advanced-->
         <!-- <div class="col-xl-6"> -->
         <div class="card">
             <h5 class="card-header">Progres</h5>
             <div class="card-body">
-                <ul class="timeline pt-3">
-                    <li class="timeline-item pb-4 timeline-item-primary border-left-dashed">
-                        <span class="timeline-indicator-advanced timeline-indicator-primary">
-                            <i class="bx bx-paper-plane"></i>
-                        </span>
-                        <div class="timeline-event">
-                            <div class="timeline-header border-bottom mb-3">
-                                <h6 class="mb-0">Ajukan Proposal</h6>
-                                <span>3rd October</span>
-                            </div>
+                <div class="table-responsive">
                             <!--  -->
-                            <div class="card border-0 shadow-sm rounded">
-                                <div class="card-body">
+                            <div class="card border-0 shadow-sm rounded full-width">
                                     <table class="table table-bordered">
                                         <thead>
                                             <tr>
                                                 <th scope="col">JENIS PENELITIAN</th>
-                                                <th scope="col">KATEGORI PENELITIAN</th>
-                                                <th scope="col">TEMA PENELITIAN</th>
                                                 <th scope="col">TOPIK PENELITIAN</th>
                                                 <th scope="col">JUDUL PENELITIAN</th>
-                                                <th scope="col">JENIS TKT</th>
-                                                <th scope="col">TARGET UTAMA RISET</th>
-                                                <!-- <th scope="col">FILE</th> -->
+                                                <th scope="col">TIM PENELITIAN</th>
+                                                <th scope="col">STATUS</th>
                                                 <th scope="col">AKSI</th>
+
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <tr>
-                                                <td class="text-center">
-                                                    <img src="" class="rounded" style="width: 150px">
-                                                </td>
                                                 <td></td>
                                                 <td></td>
                                                 <td></td>
                                                 <td></td>
                                                 <td></td>
-                                                <td></td>
-                                                <!-- <td></td> -->
                                                 <td class="text-center">
                                                     <a href="#modalToggle" data-bs-toggle="modal"
                                                         data-bs-target="#modalToggle" class="bx bx-show-alt badge-dark"></a>
@@ -139,12 +149,14 @@
                                                             class="bx bxs-edit"></i></a>
                                                     <a class=" text-danger" title="Hapus" style="cursor:pointer"
                                                         onclick=""><i class="bx bx-trash"></i></a>
+                                                    <a class=" text-danger" title="Reviewers" style="cursor:pointer"
+                                                        onclick=""><i class="bx bx-user-plus"></i></a>
                                                 </td>
                                             </tr>
                                         </tbody>
                                     </table>
-                                </div>
                             </div>
+                </div>
                             <br>
                             <div class="timeline-header mb-3">
                                 <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas"
@@ -154,9 +166,7 @@
                                         <span>Ajukan Hibah</span></span>
                                 </button>
                                 <span>
-                                    <div>
-                                        <span>6:30 AM</span>
-                                    </div>
+
                             </div>
                             <div class="offcanvas offcanvas-end @if ($errors->all()) show @endif"
                                 tabindex="-1" id="newrecord" aria-labelledby="offcanvasEndLabel">
@@ -352,170 +362,9 @@
 
                                 </div>
                             </div>
-                        </div>
-                    </li>
-                    <li class="timeline-item pb-4 timeline-item-success border-left-dashed">
-                        <span class="timeline-indicator-advanced timeline-indicator-success">
-                            <i class="bx bx-paint"></i>
-                        </span>
-                        <div class="timeline-event">
-                            <div class="timeline-header mb-sm-0 mb-3">
-                                <h6 class="mb-0">Design Review</h6>
-                                <span class="text-muted">4th October</span>
-                            </div>
-                            <p>
-                                Weekly review of freshly prepared design for our new
-                                application.
-                            </p>
-                            <div class="d-flex justify-content-between">
-                                <h6>New Application</h6>
-                                <div class="d-flex">
-                                    <div class="avatar avatar-xs me-2">
-                                        <img src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/4.png"
-                                            alt="Avatar" class="rounded-circle">
-                                    </div>
-                                    <div class="avatar avatar-xs">
-                                        <img src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/5.png"
-                                            alt="Avatar" class="rounded-circle">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="timeline-item pb-4 timeline-item-danger border-left-dashed">
-                        <span class="timeline-indicator-advanced timeline-indicator-danger">
-                            <i class="bx bx-shopping-bag"></i>
-                        </span>
-                        <div class="timeline-event">
-                            <div class="d-flex flex-sm-row flex-column">
-                                <img src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/elements/16.jpg"
-                                    class="rounded me-3" alt="Shoe img" height="62" width="62">
-                                <div>
-                                    <div class="timeline-header flex-wrap mb-2 mt-3 mt-sm-0">
-                                        <h6 class="mb-0">Sold Puma POPX Blue Color</h6>
-                                        <span class="text-muted">5th October</span>
-                                    </div>
-                                    <p>
-                                        PUMA presents the latest shoes from its collection. Light &amp;
-                                        comfortable made with highly durable material.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="d-flex justify-content-between flex-wrap flex-sm-row flex-column text-center">
-                                <div class="mb-sm-0 mb-2">
-                                    <p class="mb-0">Customer</p>
-                                    <span class="text-muted">Micheal Scott</span>
-                                </div>
-                                <div class="mb-sm-0 mb-2">
-                                    <p class="mb-0">Price</p>
-                                    <span class="text-muted">$375.00</span>
-                                </div>
-                                <div>
-                                    <p class="mb-0">Quantity</p>
-                                    <span class="text-muted">1</span>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="timeline-item pb-4 timeline-item-info border-left-dashed">
-                        <span class="timeline-indicator-advanced timeline-indicator-info">
-                            <i class="bx bx-user-circle"></i>
-                        </span>
-                        <div class="timeline-event">
-                            <div class="timeline-header">
-                                <h6 class="mb-0">Interview Schedule</h6>
-                                <span class="text-muted">6th October</span>
-                            </div>
-                            <p>
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                Possimus quos, voluptates voluptas rem veniam expedita.
-                            </p>
-                            <hr>
-                            <div class="d-flex justify-content-between flex-wrap gap-2">
-                                <div class="d-flex flex-wrap">
-                                    <div class="avatar me-3">
-                                        <img src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/6.png"
-                                            alt="Avatar" class="rounded-circle">
-                                    </div>
-                                    <div>
-                                        <p class="mb-0">Rebecca Godman</p>
-                                        <span class="text-muted">Javascript Developer</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-wrap align-items-center cursor-pointer">
-                                    <i class="bx bx-message-rounded-dots me-2"></i>
-                                    <i class="bx bx-phone-call"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="timeline-item pb-4 timeline-item-dark border-left-dashed">
-                        <span class="timeline-indicator-advanced timeline-indicator-dark">
-                            <i class="bx bx-bell"></i>
-                        </span>
-                        <div class="timeline-event">
-                            <div class="timeline-header">
-                                <h6 class="mb-0">2 Notifications</h6>
-                                <span class="text-muted">7th October</span>
-                            </div>
-                            <ul class="list-group list-group-flush">
-                                <li
-                                    class="list-group-item d-flex justify-content-between align-items-center flex-wrap border-top-0 p-0">
-                                    <div class="d-flex flex-wrap align-items-center">
-                                        <ul
-                                            class="list-unstyled users-list d-flex align-items-center avatar-group m-0 my-3 me-2">
-                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom"
-                                                data-bs-placement="top" class="avatar avatar-xs pull-up"
-                                                aria-label="Vinnie Mostowy" data-bs-original-title="Vinnie Mostowy">
-                                                <img class="rounded-circle"
-                                                    src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/5.png"
-                                                    alt="Avatar">
-                                            </li>
-                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom"
-                                                data-bs-placement="top" class="avatar avatar-xs pull-up"
-                                                aria-label="Allen Rieske" data-bs-original-title="Allen Rieske">
-                                                <img class="rounded-circle"
-                                                    src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/12.png"
-                                                    alt="Avatar">
-                                            </li>
-                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom"
-                                                data-bs-placement="top" class="avatar avatar-xs pull-up"
-                                                aria-label="Julee Rossignol" data-bs-original-title="Julee Rossignol">
-                                                <img class="rounded-circle"
-                                                    src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/6.png"
-                                                    alt="Avatar">
-                                            </li>
-                                        </ul>
-                                        <span>Commented on your post.</span>
-                                    </div>
-                                    <button class="btn btn-outline-primary btn-sm my-sm-0 my-3">
-                                        View
-                                    </button>
-                                </li>
-                                <li
-                                    class="list-group-item d-flex justify-content-between align-items-center flex-wrap pb-0 px-0">
-                                    <div class="d-flex flex-sm-row flex-column align-items-center">
-                                        <img src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/4.png"
-                                            class="rounded-circle me-3" alt="avatar" height="24" width="24">
-                                        <div class="user-info">
-                                            <p class="my-0">Dwight repaid you</p>
-                                            <span class="text-muted">30 minutes ago</span>
-                                        </div>
-                                    </div>
-                                    <h5 class="mb-0">$20</h5>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="timeline-end-indicator">
-                        <i class="bx bx-check-circle"></i>
-                    </li>
-                </ul>
-            </div>
         </div>
     </div>
     <!-- /Timeline Advanced-->
-    </div>
 
     <!-- </div> -->
 
