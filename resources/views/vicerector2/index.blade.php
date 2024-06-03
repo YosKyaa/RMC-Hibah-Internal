f0956a0a067375b4f6d5920a503bef3e5e00f9cc
Abay <94115323+Adamrakb@users.noreply.github.com>
2024-06-03 16:07:40 +0700

    vice rector 1 & 2

--- resources/views/vicerector2/index.blade.php
+++ resources/views/vicerector2/index.blade.php
@@ -38,7 +38,7 @@
                         <div class="d-flex align-items-end row">
                             <div class="col-sm-5 text-center text-sm-left">
                                 <div class="card-body pb-0">
-                                <img src="../../assets/img//man-with-laptop-light.png" height="140" alt="View Badge User" 
+                                <img src="../../assets/img/man-with-laptop-light.png" height="140" alt="View Badge User" 
                                     data-app-light-img="man-with-laptop-light.png" 
                                         data-app-dark-img="man-with-laptop-dark.png">
                                 </div>
@@ -50,34 +50,108 @@
                                         job.</p>
 
                                     <span class="badge bg-label-primary">78% of target</span>
+                                    <div class="d-flex align-items-center justify-content-between app-academy-md-80">
+                                        <input type="search" placeholder="Find your course" class="form-control me-2">
+                                            <button type="submit" class="btn btn-primary btn-icon"><i class="bx bx-search"></i></button>
+                                    </div>
                                 </div>
                             </div>
+                            
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="card full-width">
-                <div class="container">
-                    <table class="table table-hover table-sm" id="datatable" width="50%">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Nama Dosen</th>
-                                <th>Jenis Penelitian</th>
-                                <th>Bidang Penelitian</th>
-                                <th>Judul Penelitian</th>
-                                <th>Tim Peneliti</th>
-                                <th>Jenis TKT</th>
-                                <th>Target Utama Riset</th>
-                                <th>Lampiran Dokumen</th>
-                                <th>Status</th>
-                                <th>Note</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
+
+            <div class="card mb-4" data-select2-id="7">
+    <div class="card-header d-flex flex-wrap justify-content-between gap-3" data-select2-id="6">
+      <div class="card-title mb-0 me-1">
+        <h5 class="mb-1">My Courses</h5>
+        <p class="text-muted mb-0">Total 6 course you have purchased</p>
+      </div>
+      <div class="d-flex justify-content-md-end align-items-center gap-3 flex-wrap" data-select2-id="5">
+        <div class="position-relative" data-select2-id="4"><select id="select2_course_select" class="select2 form-select select2-hidden-accessible" data-placeholder="All Courses" data-select2-id="select2_course_select" tabindex="-1" aria-hidden="true">
+          <option value="" data-select2-id="2">All Courses</option>
+          <option value="all courses" data-select2-id="16">All Courses</option>
+          <option value="ui/ux" data-select2-id="17">UI/UX</option>
+          <option value="seo" data-select2-id="18">SEO</option>
+          <option value="web" data-select2-id="19">Web</option>
+          <option value="music" data-select2-id="20">Music</option>
+          <option value="painting" data-select2-id="21">Painting</option>
+        </select>
+        <span class="select2 select2-container select2-container--default select2-container--below" dir="ltr" data-select2-id="1" style="width: 126px;">
+        <span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-disabled="false" aria-labelledby="select2-select2_course_select-container">
+        <span class="select2-selection__rendered w-px-150" id="select2-select2_course_select-container" role="textbox" aria-readonly="true">
+            <span class="select2-selection__placeholder">All Courses</span></span>
+            <span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span>
+            <span class="dropdown-wrapper" aria-hidden="true"></span></span>
+        </div>
+
+        <label class="switch">
+          <input type="checkbox" class="switch-input">
+          <span class="switch-toggle-slider">
+            <span class="switch-on"></span>
+            <span class="switch-off"></span>
+          </span>
+          <span class="switch-label text-nowrap mb-0">Hide completed</span>
+        </label>
+      </div>
+    </div>
+
+    <div class="card-body">
+      <div class="row gy-4 mb-4">
+        <div class="col-sm-6 col-lg-4">
+          <div class="card p-2 h-100 shadow-none border">
+            <div class="card-body p-3 pt-2">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <span class="badge bg-label-primary">Web</span>
+              </div>
+              <a href="app-academy-course-details.html" class="h5">JUDUL</a>
+              <p class="mt-2">CATATAN</p>
+              <a href="../link">
+                <i class="bx bx-link"></i>
+                Document
+              </a>
+              <br> <br>
+              <div class="d-flex flex-column flex-md-row gap-2 text-nowrap pe-xl-3 pe-xxl-0">
+                <a class="app-academy-md-50 btn btn-label-secondary me-md-2 d-flex align-items-center" href="app-academy-course-details.html">
+                  <i class="bx bx-sync align-middle me-1"></i><span>Disapprove</span>
+                </a>
+                <a class="app-academy-md-50 btn btn-label-primary d-flex align-items-center" href="app-academy-course-details.html">
+                  <span class="me-1">Approve</span><i class="bx bx-chevron-right lh-1 scaleX-n1-rtl"></i>
+                </a>
+              </div>
             </div>
+          </div>
+        </div>
+        <!--  -->
+      </div>
+      <nav aria-label="Page navigation" class="d-flex align-items-center justify-content-center">
+        <ul class="pagination">
+          <li class="page-item prev">
+            <a class="page-link" href="javascript:void(0);"><i class="tf-icon bx bx-chevron-left"></i></a>
+          </li>
+          <li class="page-item active">
+            <a class="page-link" href="javascript:void(0);">1</a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="javascript:void(0);">2</a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="javascript:void(0);">3</a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="javascript:void(0);">4</a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="javascript:void(0);">5</a>
+          </li>
+          <li class="page-item next">
+            <a class="page-link" href="javascript:void(0);"><i class="tf-icon bx bx-chevron-right"></i></a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
         </div>
         <!-- / Content -->
 
@@ -167,7 +241,7 @@
                         render: function(data, type, row, meta) {
                             var html =
                                 `<a class=" text-success" title="Edit" href="{{ url('setting/manage_studyprogram/studyprogram/edit/` +
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                row.id + `') }}"><i class="bx bxs-edit"></i></a> 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                row.id + `') }}"><i class="bx bxs-edit"></i></a> 
                             <a class=" text-danger" title="Hapus" style="cursor:pointer" onclick="DeleteId(\'` + row
                                 .id + `\',\'` + row.name + `\')" ><i class="bx bx-trash"></i></a>`;
                             return html;
