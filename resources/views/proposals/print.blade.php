154085a700b6750fbadafa1b5fee252d93fcee11
Abay <94115323+Adamrakb@users.noreply.github.com>
2024-06-26 14:17:07 +0700

    dompdf fixed

--- resources/views/proposals/print.blade.php
+++ resources/views/proposals/print.blade.php
@@ -1,34 +1,194 @@
-Nomor : 012/L4/SP2/IX/JGU/2024
-Lampiran : 1 lembar
-Hal : Pengumuman Penerimaan Pendanaan Program Hibah Internal
-Universitas Global Jakarta
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Kontrak Penelitian</title>
+    <style>
+        
+        body { font-family: "Times New Roman", Times, serif; }
+        .container { width: 80%; margin: auto; }
+        .header { display: flex; justify-content: space-between; align-items: center; }
+        .header img { max-width: 150px; }
+        h1, h2, h3, h4 { text-align: center; }
+        .content { margin: 20px 0; }
+        .content p { text-align: justify; }
+        td, li {text-align: justify;}
+        th{text-align: justify;}
+        .ttd{ width: 100%; text-align: center; }
+        .ttd p {text-align: center;}
+        .center { width: 100%; text-align: center; margin-top: 20px; }
+        .center p {text-align: center;}
 
-Yth.
-Bapak/Ibu Dosen Universitas Global Jakarta
-di-tempat
+  header {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    height: 30px;
+    text-align: right;
+    background-color: #f0f0f0;
+  }
+  footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    height: 40px;
+    text-align: center;
+    background-color: #f0f0f0;
+  }
+</style>
 
-Berdasarkan hasil seleksi proposal dari Tim Reviewer Internal JGU menetapkan penerima hibah internal penelitian yang
-ditetapkan melalui Surat Keputusan Rektor Universitas Global Jakarta Nomor : 91/SK/JGU/VI/2024 tanggal 08 Juni 2024
-tentang Penerima Program Hibah Internal Penelitian Tahun 2024, maka bersama ini kami sampaikan daftar nama terlampir.
-Berkenaan dengan hal tersebut, Research Management Centre mengucapkan selamat kepada para Penerima Program Hibah
-Internal Penelitian Tahun 2024. Research Management Centre mengucapkan terima kasih kepada pengusul yang telah
-berpartisipasi dan apabila nama pengusul tidak tercantum dalam lampiran, maka dapat mengusulkan kembali pada tahun yang
-akan datang.
-Berkaitan dengan hal ini, dapat kami sampaikan beberapa hal sebagai berikut yaitu:
-1. Bagi pengusul yang dinyatakan lolos dan menerima pendanaan hibah untuk dapat segera merevisi proposal hasil review
-sebagaimana yang tertulis dalam catatan review. Proposal revisi dapat diupload di www.rmc.jgu.ac.id. Mulai tanggal 10
-s/d 16 Juni 2024 pukul 16.00 WIB. Jika pada sampai batas waktu pengumpulan proposal revisi, calon penerima hibah belum
-mengumpulkan maka pengusul akan dinyatakan gugur.
-2. Penandatanganan kontrak pelaksanaan program penelitian akan diinformasikan lebih lanjut.
-3. Hal-hal lain yang terkait dengan penandatanganan kontrak, pencairan dana dan pelaksanaan program penelitian akan
-diinformasikan lebih lanjut melalui web RMC (www.rmc.jgu.ac.id).
-Demikian surat ini kami buat, atas perhatian dan kerjasamanya diucapkan terima kasih.
+</head>
+<body>
+<header>
+    <img src="{{ public_path('assets/img/picture1.jgp') }}">
+</header>
+<footer>
+    <img src="{{ public_path('assets/img/picture2.jgp') }}">
+</footer>
+  <div class="container">
+    <hr style="height: 2px; background-color: black;">
+  </div>
+    <div class="container">
+        <div class="header">
+            <div>
+                <h3>KONTRAK PENELITIAN <br> PENELITIAN HIBAH UNIVERSITAS GLOBAL JAKARTA</h3>
+                <h4>Tahun Anggaran 2023 <br> Nomor : 013/L4/SK/VII/JGU/2023</h4>
+            </div>
+        </div>
+        <div class="content">
+        <p>Pada hari ini Rabu tanggal 12 bulan Juli tahun Dua Ribu Dua Puluh Tiga, kami yang bertandatangan di bawah ini:</p>
+                    
+        <table>
+            <thead>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th>1. Anugerah B. Adina, S.Farm., M.H.Sc. <br> <br> <br>  </th>
+                <td>: Kepala <i>Research Management Centre</i> (Pusat Pengelola Penelitian) Universitas Global Jakarta, 
+                        dalam hal ini bertindak untuk dan atas nama Universitas Global Jakarta, yang berkedudukan di 
+                        Grand Depok City, Jl. Boulevard Raya No. 2, Kota Depok, 16412, Jawa Barat, Indonesia. Untuk 
+                        selanjutnya disebut <b>PIHAK PERTAMA.</b></td>
+            </tr>
+            <tr>
+                <th>2. Safira Faizah, S.Tr.Kom., M.IT </th>
+                <td>: Dosen Program Studi Teknik Informatika Universitas Global Jakarta, dalam hal ini bertindak 
+                sebagai pengusul dan Ketua Pelaksana Penelitian Tahun Anggaran 2023 untuk selanjutnya disebut 
+                <b>PIHAK KEDUA.</b></td>
+            </tr>
+            </tbody>
+        </table>
+        <table>
+                
+        </table>
 
+        <p><AK>PIHAK PERTAMA</b> dan <b>PIHAK KEDUA</b> secara bersama-sama sepakat mengikatkan diri dalam suatu Kontrak Penelitian Hibah Universitas Global Jakarta Tahun Anggaran 2023 dengan ketentuan dan syarat-syarat sebagai berikut:</p>
 
-Depok, 06 Juni 2024
-Kepala RMC
+<h4>PASAL 1 <br> RUANG LINGKUP KONTRAK</h4>
+<p><b>PIHAK PERTAMA</b> memberi pekerjaan kepada <b>PIHAK KEDUA</b> dan <b>PIHAK KEDUA</b> menerima pekerjaan tersebut dari <b>PIHAK PERTAMA</b> untuk melaksanakan dan menyelesaikan Penelitian Hibah Universitas Global Jakarta Tahun Anggaran 2023 dengan judul “Sistem Deteksi Penyakit Tremor Istirahat Pada Penderita Parkinson Menggunakan Internet of Things (IoT)”.</p>
 
+<h4>PASAL 2 <br> DANA PENELITIAN</h4>
+<p>Besarnya dana untuk melaksanakan penelitian dengan judul sebagaimana dimaksud pada Pasal 1 adalah sebesar Rp. 14.500.000.- (Empat Belas Juta Lima Ratus Ribu Rupiah) sudah termasuk pajak. Dana Penelitian sebagaimana dimaksud pada ayat (1) dibebankan pada Dana Penelitian Hibah Universitas Global Jakarta.</p>
 
+<h4>PASAL 3 <br> TATA CARA PEMBAYARAN DANA PENELITIAN</h4>
+    <p>(1) <AK>PIHAK PERTAMA</b> akan membayarkan Dana Penelitian kepada <b>PIHAK KEDUA</b> secara bertahap dengan ketentuan sebagai berikut:</p>
+    <ul>
+        <li>Pembayaran Tahap Pertama sebesar 70% dari total dana penelitian yaitu 70% x Rp.14.500.000,- = Rp.10.150.000,- (Sepuluh Juta Seratus Lima Puluh Ribu Rupiah), yang akan dibayarkan oleh <b>PIHAK PERTAMA</b> kepada <b>PIHAK KEDUA</b> setelah <b>PIHAK KEDUA</b> menyerahkan proposal usulan yang sudah direvisi sebanyak 4 (empat) eksemplar</li> <br>
+        <li>Pembayaran Tahap Kedua sebesar 30% dari total dana penelitian yaitu 30% x Rp.14.500.000,- = Rp.4.350.000,- (Empat Juta Tiga Ratus Lima Puluh Ribu Rupiah), dibayarkan oleh <b>PIHAK PERTAMA</b> kepada <b>PIHAK KEDUA</b> setelah <b>PIHAK KEDUA</b> mengunggah Laporan akhir Pelaksanaan Penelitian serta memasukkan hardcopy laporan akhir dan bukti luaran penelitian.</li>
+    </ul>
+    <p>(2) Dana Penelitian sebagaimana dimaksud pada ayat (1) akan disalurkan oleh <b>PIHAK PERTAMA</b> kepada <b>PIHAK KEDUA</b> ke rekening sebagai berikut:</p>
+    <p>Nama : Safira Faizah<br>
+                Nomor Rekening : 0004801580007700<br>
+                Nama Bank : Bank BTN</p>
+    <p>(3) <AK>PIHAK PERTAMA</b> tidak bertanggung jawab atas keterlambatan dan/atau tidak terbayarnya sejumlah dana sebagaimana dimaksud pada ayat (1) yang disebabkan karena kesalahan <b>PIHAK KEDUA</b> dalam menyampaikan data peneliti, nama bank, nomor rekening, dan persyaratan lainnya yang tidak sesuai dengan ketentuan.</p>
 
-Anugerah B. A, S.Farm., M.H.Sc.
-NIDN: 0422028905
+<h4>PASAL 4 <br> JANGKA WAKTU</h4>
+<p>Jangka waktu pelaksanaan penelitian sebagaimana dimaksud dalam Pasal 1 sampai selesai 100% adalah tanggal 10 Desember 2023.</p>
+
+<h4>PASAL 5 <br> TARGET LUARAN</h4>
+<p><b>PIHAK KEDUA</b> berkewajiban untuk mencapai target luaran wajib penelitian berupa satu artikel di jurnal internasional terindeks pada basis data internasional bereputasi; atau satu artikel di jurnal internasional; atau satu artikel di jurnal nasional terakreditasi SINTA 1 sampai 6. <b>PIHAK KEDUA</b> berkewajiban untuk melaporkan perkembangan pencapaian target luaran sebagaimana dimaksud pada ayat (1) kepada <b>PIHAK PERTAMA</b>.</p>
+
+<h4>PASAL 6 <br> HAK DAN KEWAJIBAN PARA PIHAK</h4>
+<p>Hak dan Kewajiban <b>PIHAK PERTAMA</b>:</p>
+<ul>
+    <li><b>PIHAK PERTAMA</b> berhak untuk mendapatkan dari <b>PIHAK KEDUA</b> laporan akhir dan bukti luaran penelitian.</li>
+    <li><b>PIHAK PERTAMA</b> berkewajiban untuk memberikan dana penelitian kepada <b>PIHAK KEDUA</b> dengan jumlah sebagaimana dimaksud dalam Pasal 2 ayat (1) dan dengan tata cara pembayaran sebagaimana dimaksud dalam Pasal 3.</li>
+</ul>
+
+<p>Hak dan Kewajiban <b>PIHAK KEDUA</b>:</p>
+<ul>
+    <li><b>PIHAK KEDUA</b> berhak menerima dana penelitian dari <b>PIHAK PERTAMA</b> dengan jumlah sebagaimana dimaksud dalam Pasal 2 ayat (1).</li>
+    <li><b>PIHAK KEDUA</b> berkewajiban menyerahkan kepada <b>PIHAK PERTAMA</b> laporan sebagaimana dimaksud pada pasal 3 ayat (1b) Penelitian Hibah Universitas Global Jakarta dengan judul Sistem Deteksi Penyakit Tremor Istirahat Pada Penderita Parkinson Menggunakan Internet of Things (IoT).</li>
+    <li><b>PIHAK KEDUA</b> berkewajiban untuk bertanggung jawab dalam penggunaan dana penelitian yang diterimanya sesuai dengan proposal kegiatan yang telah disetujui.</li>
+    <li><b>PIHAK KEDUA</b> berkewajiban untuk menyampaikan kepada <b>PIHAK PERTAMA</b> laporan penggunaan dana sebagaimana dimaksud dalam Pasal 7.</li>
+</ul>
+
+<h4>PASAL 7 <br> LAPORAN PELAKSANAAN PENELITIAN</h4>
+<p><b>PIHAK KEDUA</b> berkewajiban untuk menyampaikan kepada <b>PIHAK PERTAMA</b> berupa laporan kemajuan dan laporan akhir serta luaran penelitian dan rekapitulasi penggunaan anggaran sesuai dengan jumlah dana yang diberikan oleh <b>PIHAK PERTAMA</b> yang tersusun secara sistematis sesuai pedoman yang ditentukan oleh <b>PIHAK PERTAMA</b>. <b>PIHAK KEDUA</b> berkewajiban mengunggah/menyerahkan Laporan Kemajuan dan Surat Pernyataan Tanggungjawab Belanja (SPTB) paling lambat 08 September 2023. <b>PIHAK KEDUA</b> berkewajiban menyerahkan laporan akhir bukti luaran paling lambat 01 Desember 2023. Laporan hasil Penelitian sebagaimana tersebut pada ayat (4) harus memenuhi ketentuan sebagai berikut: Bentuk/ukuran kertas A4; Di bawah bagian cover ditulis: Dibiayai oleh: Dana Penelitian Hibah Universitas Global Jakarta Kementerian Pendidikan Kebudayaan Riset & Teknologi Nomor SK 013/L4/SK/VII/JGU/2023 Tanggal 12 Juli 2023</p>
+
+<h4>PASAL 8 <br> MONITORING DAN EVALUASI</h4>
+<p><b>PIHAK PERTAMA</b> dalam rangka pengawasan akan melakukan Monitoring dan Evaluasi pada bulan September 2023 terhadap kemajuan pelaksanaan Penelitian Tahun Anggaran 2023.</p>
+
+<h4>PASAL 9 <br> PENILAIAN LUARAN</h4>
+<p>Penilaian luaran penelitian dilakukan oleh Komite Penilai/Reviewer Luaran sesuai dengan ketentuan yang berlaku.</p>
+
+<h4>PASAL 10 <br> PERUBAHAN SUSUNAN TIM PELAKSANA DAN SUBSTANSI PELAKSANAAN</h4>
+<p>Perubahan terhadap susunan tim pelaksana dan substansi pelaksanaan Penelitian ini dapat dibenarkan apabila telah mendapat persetujuan tertulis dari Kepala Research Management Centre (Pusat Pengelola Penelitian) Universitas Global Jakarta.</p>
+
+<h4>PASAL 11 <br> PENGGANTIAN KETUA PELAKSANA</h4>
+<p>Apabila <AK>PIHAK KEDUA</b> selaku ketua pelaksana tidak dapat melaksanakan Penelitian ini maka <b>PIHAK KEDUA</b> wajib mengusulkan pengganti ketua pelaksana yang merupakan salah satu anggota tim kepada <b>PIHAK PERTAMA</b>. Apabila <b>PIHAK KEDUA</b> tidak dapat melaksanakan tugas dan tidak ada pengganti ketua sebagaimana dimaksud pada ayat (1) maka <b>PIHAK KEDUA</b> harus mengembalikan dana penelitian kepada <b>PIHAK PERTAMA</b> yang selanjutnya disetor ke Kas Negara. Bukti setor sebagaimana dimaksud pada ayat (2) disimpan oleh <b>PIHAK PERTAMA</b>.</p>
+
+<h4>PASAL 12 <br> SANKSI</h4>
+<p>Apabila sampai dengan batas waktu yang telah ditetapkan untuk melaksanakan Penelitian ini telah berakhir namun <b>PIHAK KEDUA</b> belum menyelesaikan tugasnya terlambat mengirim laporan Kemajuan dan/atau terlambat mengirim laporan akhir maka <b>PIHAK KEDUA</b> dikenakan sanksi administratif berupa penghentian pembayaran dan tidak dapat mengajukan proposal penelitian dalam kurun waktu dua tahun berturut-turut. Apabila <b>PIHAK KEDUA</b> tidak dapat mencapai target luaran sebagaimana dimaksud dalam Pasal 5 maka kekurangan capaian target luaran berdampak tidak dibayarkan dana 30 %  dan wajib mengembalikan dana 70 % yang telah diterima.</p>
+
+<h4>PASAL 13 <br> PEMBATALAN PERJANJIAN</h4>
+<p>Apabila dikemudian hari terhadap judul Penelitian sebagaimana dimaksud dalam Pasal 1 ditemukan adanya duplikasi dengan Penelitian lain dan/atau ditemukan adanya ketidakjujuran itikad tidak baik dan/atau perbuatan yang tidak sesuai dengan kaidah ilmiah dari atau dilakukan oleh <b>PIHAK KEDUA</b> maka perjanjian Penelitian ini dinyatakan batal dan <b>PIHAK KEDUA</b> wajib mengembalikan dana penelitian yang telah diterima kepada <b>PIHAK PERTAMA</b> yang selanjutnya akan disetor ke Kas Negara. Bukti setor sebagaimana dimaksud pada ayat (1) disimpan oleh <b>PIHAK PERTAMA</b>.</p>
+
+<h4>PASAL 14 <br> PAJAK-PAJAK</h4>
+<p>Hal-hal dan/atau segala sesuatu yang berkenaan dengan kewajiban pajak berupa PPN dan/atau PPh menjadi tanggung jawab <b>PIHAK KEDUA</b> dan harus dibayarkan oleh <b>PIHAK KEDUA</b> ke kantor pelayanan pajak setempat sesuai ketentuan yang berlaku.</p>
+
+<h4>PASAL 15 <br> PERALATAN DAN/ALAT HASIL PENELITIAN</h4>
+<p>Hasil Pelaksanaan Penelitian ini yang berupa peralatan dan/atau alat yang dibeli dari pelaksanaan Penelitian ini adalah milik Negara yang dapat dihibahkan kepada Universitas Global Jakarta sesuai dengan ketentuan peraturan perundang-undangan.</p>
+
+<h4>PASAL 16 <br> PENYELESAIAN SENGKETA</h4>
+<p>Apabila terjadi perselisihan antara <b>PIHAK PERTAMA</b> dan <b>PIHAK KEDUA</b> dalam pelaksanaan perjanjian ini akan dilakukan penyelesaian secara musyawarah dan mufakat dan apabila tidak tercapai penyelesaian secara musyawarah dan mufakat maka penyelesaian dilakukan melalui proses hukum.</p>
+
+<h4>PASAL 17 <br> LAIN-LAIN</h4>
+<p><b>PIHAK KEDUA</b> menjamin bahwa penelitian dengan judul tersebut di atas belum pernah dibiayai dan/atau diikutsertakan pada Pendanaan Penelitian lainnya baik yang diselenggarakan oleh instansi lembaga perusahaan atau yayasan baik di dalam maupun di luar negeri. Segala sesuatu yang belum cukup diatur dalam Perjanjian ini dan dipandang perlu diatur lebih lanjut dan dilakukan perubahan oleh PARA PIHAK maka perubahan-perubahannya akan diatur dalam perjanjian tambahan atau perubahan yang merupakan satu kesatuan dan bagian yang tidak terpisahkan dari Perjanjian ini. Perjanjian ini dibuat dan ditandatangani oleh PARA PIHAK pada hari dan tanggal tersebut di atas dibuat dalam rangkap 2 (dua) dan bermeterai cukup sesuai dengan ketentuan yang berlaku yang masing-masing mempunyai kekuatan hukum yang sama.</p>
+
+<table class="ttd">
+    <tr>
+        <td>
+        <p>PIHAK PERTAMA</p>
+        <br> 
+        <br> 
+        <br>
+        <p>Anugerah B. Adina S.Farm. M.H.Sc.<br>NIDN: 0422028905</p>
+        </td>
+        <td>
+        <p>PIHAK KEDUA</p>
+        <br> 
+        <br> 
+        <br> 
+        <p>Safira Faizah S.Tr.Kom. M.IT<br>NIDN: 0427059601</p>
+        <tr>
+</table>
+
+    <div class="center">
+        <p>Mengetahui<br>
+        WAKIL REKTOR I<br> 
+        <br> 
+        <br> 
+        <br> 
+        <br>
+        Adhes Gamayel Ph.D<br>NIDN: 0322118204</p>
+    </div>
+    </div>
+</body>
+</html>
